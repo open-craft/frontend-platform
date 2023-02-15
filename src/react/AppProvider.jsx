429694d9a702cd29d7b4a5327198fff4d96027de
Adam Stankiewicz <agstanki@gmail.com>
2023-02-15 17:43:19 -0500

    feat: adds support for loading external theme CSS for MFEs
    Introduces `useAppTheme` in `AppProvider` to load/inject `<link>` elements for the core theme CSS and any theme variant CSS into the HTML document. Exposes the app theme state and a way to mutate it to consumers via `AppContext`.

--- src/react/AppProvider.jsx
+++ src/react/AppProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -6,7 +6,11 @@ import OptionalReduxProvider from './OptionalReduxProvider';
 
 import ErrorBoundary from './ErrorBoundary';
 import AppContext from './AppContext';
-import { useAppEvent, useTrackColorSchemeChoice } from './hooks';
+import { useAppEvent, useAppTheme, useTrackColorSchemeChoice } from './hooks';
+import {
+  APP_THEME_CORE,
+  APP_THEME_LIGHT,
+} from './constants';
 import { getAuthenticatedUser, AUTHENTICATED_USER_CHANGED } from '../auth';
 import { getConfig } from '../config';
 import { CONFIG_CHANGED } from '../constants';
@@ -48,7 +52,6 @@ export default function AppProvider({ store, children, wrapWithRouter }) {
   const [config, setConfig] = useState(getConfig());
   const [authenticatedUser, setAuthenticatedUser] = useState(getAuthenticatedUser());
   const [locale, setLocale] = useState(getLocale());
-  const [themeLoaded, setThemeLoaded] = useState(false);
 
   useTrackColorSchemeChoice();
 
@@ -64,25 +67,26 @@ export default function AppProvider({ store, children, wrapWithRouter }) {
     setLocale(getLocale());
   });
 
-  useEffect(() => {
-    if (config.THEME_OVERRIDE_URL) {
-      const themeLink = document.createElement('link');
-      themeLink.href = config.THEME_OVERRIDE_URL;
-      themeLink.rel = 'stylesheet';
-      themeLink.type = 'text/css';
-      themeLink.onload = () => setThemeLoaded(true);
-      themeLink.onerror = () => setThemeLoaded(true);
+  const [appThemeState, appThemeDispatch] = useAppTheme({
+    themeUrls: {
+      [APP_THEME_CORE]: config.APP_THEME_CORE_URL,
+      variants: {
+        [APP_THEME_LIGHT]: config.APP_THEME_LIGHT_URL,
+      },
+    },
+  });
 
-      document.head.appendChild(themeLink);
+  const appContextValue = useMemo(() => ({
+    authenticatedUser,
+    config,
+    locale,
+    appTheme: {
+      state: appThemeState,
+      dispatch: appThemeDispatch,
+    },
+  }), [authenticatedUser, config, locale, appThemeState, appThemeDispatch]);
 
-      return () => document.head.removeChild(themeLink);
-    }
-    setThemeLoaded(true);
-  }, [config.THEME_OVERRIDE_URL]);
-
-  const appContextValue = useMemo(() => ({ authenticatedUser, config, locale }), [authenticatedUser, config, locale]);
-
-  if (!themeLoaded) {
+  if (!appThemeState?.isThemeLoaded) {
     return null;
   }
 
